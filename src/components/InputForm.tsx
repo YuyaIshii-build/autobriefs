af419742efe2863a637dfba5b5b3eb9e98a9ada3
Yuriya Ishikawa <yuriyaishikawa@Mac.homenet.telecomitalia.it>
2025-06-06 15:26:07 +0200

    Fix build error in page.tsx

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -39,6 +39,7 @@ export default function InputForm({ onSubmit }: Props) {
         required
       />
 
+
       <button
         type="submit"
         className="px-4 py-2 bg-blue-600 text-white rounded disabled:opacity-50"
